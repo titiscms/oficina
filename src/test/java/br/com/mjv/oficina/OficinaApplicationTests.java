6dbc13c36bf9de9c411433b109854342ca152b57
titiscms <titisbackup@gmail.com>
2020-11-13 00:53:43 -0300

    Configurações iniciais do projeto oficina

---
+++ src/test/java/br/com/mjv/oficina/OficinaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mjv.oficina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
